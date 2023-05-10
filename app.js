de696180eb6ec2413b88be0b09a0b9385cd86c75
divianl <divianlopezo@gmail.com>
2023-05-09 21:11:00 -0500

    index.html & css

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ const path = require('path');
 const publicPath = path.join(__dirname, './public');
 
 app.use(express.static(publicPath));
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/logim.html'))
+})
+app.get('/productcart', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/productcart.html'))
+})
+
+app.listen(3005, ()=>{
+    console.log("Servidor corriendo en el puerto 3005");
+})
