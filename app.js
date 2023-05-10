cf8a60fb00497f781434f946e3ca658558d34714
JDDL.dev <juliandariolopeztulua@gmail.com>
2023-05-10 10:08:19 -0500

    register

--- app.js
+++ app.js
@@ -8,18 +8,20 @@ const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/index.html'))
-})
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
 app.get('/login', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/login.html'))
-})
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
 app.get('/productcart', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/productcart.html'))
-})
+    res.sendFile(path.join(__dirname, '/views/productcart.html'));
+});
 app.get('/productdetail', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
-})
-
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+});
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
 app.listen(3005, ()=>{
     console.log("Servidor corriendo en el puerto 3005");
-})
+});
