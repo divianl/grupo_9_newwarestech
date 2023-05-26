b943e94e2a8f5a5181a6eb347701d0eb8f8cfb5a
divianl <divianlopezo@gmail.com>
2023-05-26 09:21:01 -0500

    funcionalidades CRUD

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+//Requiriendo modulos y asignandolos a constantes
 const express = require ('express');
 const app = express();
-
 const path = require('path');
+const methodOverride = require('method-override');
 
+// Requiriendo rutas
 const mainRoutes = require('./routes/mainRoutes');
 const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
+const productRoutes = require('./routes/productRoutes'); 
 
+// Especificando vistas
 app.set('view engine', 'ejs');
 app.set('views',[
     path.join(__dirname, './views/main'),
@@ -15,14 +18,22 @@ app.set('views',[
     path.join(__dirname, './views/users'),
 ])
 
+// Middlewares
+    //Ruta carpeta publica
 const publicPath = path.join(__dirname,'./public');
+app.use(express.static(publicPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));  
 
+//Enturadotes (routers)
 app.use(mainRoutes);
 app.use(userRoutes);
-app.use(productRoutes);
+        // /products es un prefijo
+app.use('/products', productRoutes);
 
-app.use(express.static(publicPath));
 
+//Levantando el servidor 
 app.listen(3005, ()=>{
     console.log("Servidor corriendo http://localhost:3005/");
 });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,16 +2,36 @@ const express = require('express');
 const productController = require('../controllers/productController');
 const router = express.Router();
 
+//@GET /products/createProduct
+router.get('/createProduct', productController.createProduct);
+
+//@POST /products/createProduct
+router.post('/createProduct', productController.addProduct);
+
+//@GET /products/productCart
 router.get('/productCart', productController.getCart);
 
-router.get('/productDetail', productController.getDetail);
+//@GET /products/:id/productDetail
+router.get('/:id/productDetail', productController.getDetail);
 
+//@DELETE /products/:id/delete
+router.get('/:id/delete', productController.deleteProduct);
+
+//@GET /products/:id/update
+router.get('/:id/update', productController.getUpdate);
+
+//@put /products/:id/update
+router.put('/:id/update', productController.updateProduct);
+
+//Vistas con listado de productos
+//@GET /products/productsPhones
 router.get('/productsPhones', productController.getPhones);
-
+//@GET /products/productsPrinters
 router.get('/productsPrinters', productController.getPrinters);
-
+//@GET /products/productsInformatica
 router.get('/productsInformatica', productController.getInformatica);
-
+//@GET /products/productsAccesorios
 router.get('/productsAccesorios', productController.getAccesorios);
+
 
 module.exports = router;
